56631be32ab32e9889f95bd22a39c0d4b1f314f9
Leo Tian <tian4010292@outlook.com>
2016-02-24 16:20:34 +0800

    连接mysql数据库

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var settings = require('./settings');
 
 var app = express();
 
